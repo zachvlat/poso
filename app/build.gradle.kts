d66a695a4977dfa8d0c29d5eef27fcdb47f5c3dc
zachvlat <zachariasvlatakis@gmail.com>
2026-07-17 11:31:00 +0300

    versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.zachvlat.howmuchgr"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
